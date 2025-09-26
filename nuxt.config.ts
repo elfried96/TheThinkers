646a60e9b08b035cdcba6180c3c5f30630fc3f50
Tobi3h <tobiobassandjo@gmail.com>
2025-09-26 13:25:33 +0100

    MOdifs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: false },
+  compatibilityDate: "2025-07-15",
   css: ['~/assets/css/main.css'],
   modules: [
-    '@nuxtjs/tailwindcss', // Ajoute ce module
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/ui',
     '@pinia/nuxt'
-  ]
-})
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+});
